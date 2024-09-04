38f4169f35d26281c05c6c0b923c969b079cdea3
Katy248 <petrovanton247@gmail.com>
2024-09-04 13:21:14 +0300

    Added simple server

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,34 @@
 package main
 
-import "log"
+import (
+	"cdecode/storage"
+	"database/sql"
+
+	"github.com/gin-gonic/gin"
+)
+
+const defaultUsername = "admin"
+
+var db *sql.DB
 
 func main() {
-	log.Println("Make it run or die testing")
+	// db setup
+	db = storage.InitDB()
+	defer db.Close()
+
+	// server setup
+	r := gin.Default()
+
+	// routes setup
+	r.GET("/aliases", getAliasesHandler)
+	r.GET("/users", getUsersHandler)
+
+	r.Run(":1488")
+}
+
+func getAliasesHandler(ctx *gin.Context) {
+	ctx.JSON(200, storage.GetAliases(db, defaultUsername))
+}
+func getUsersHandler(ctx *gin.Context) {
+	ctx.JSON(200, storage.GetUsers(db, defaultUsername))
 }
